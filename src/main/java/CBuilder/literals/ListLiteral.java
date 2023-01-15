871414a8bb94b84f75a93f7d2e4ddb025b17b230
Patrick Hüntelmann <patrick.huentelmann@gmail.com>
2023-01-15 06:49:16 +0100

    introduce list literal type in builder

---
+++ src/main/java/CBuilder/literals/ListLiteral.java
@@ -0,0 +1,44 @@
+package CBuilder.literals;
+
+import CBuilder.Expression;
+
+import java.util.List;
+
+/**
+ * A simple tuple literal.
+ */
+public class ListLiteral implements Literal {
+
+    /**
+     * The list of values the list is containing.
+     */
+    private final List<Expression> elems;
+
+    /**
+     * Create a new list literal with the specified values.
+     *
+     * @param elems A list of values the list should contain.
+     */
+    public ListLiteral(List<Expression> elems) {
+        this.elems = elems;
+    }
+
+    @Override
+    public String buildExpression() {
+        StringBuilder tupleInit = new StringBuilder();
+
+        for (int i = 0; i < elems.size(); i++) {
+            tupleInit.append("__mpy_list_add(" + elems.get(i).buildExpression() + ", ");
+        }
+        tupleInit.append("__mpy_obj_init_list()");
+        tupleInit.append(")".repeat(elems.size()));
+
+        return tupleInit.toString();
+    }
+
+    @Override
+    public String buildStatement() {
+        return "__mpy_obj_ref_dec(" + buildExpression() + ");\n";
+    }
+
+}
